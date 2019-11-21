0a07e10b649f9de06cfa69ed921acb52cd002f1c
Syst3mFailure <44707375+Syst3mFailure@users.noreply.github.com>
2019-11-20 22:55:56 -0500

    Add files via upload

---
+++ sudo.sh
@@ -0,0 +1,88 @@
+clear
+sleep 1
+pkg update -y
+pkg install wget -y
+wget https://informaticayhacking.cf/sudo
+pkg install toilet -y
+clear
+sleep 1
+toilet -f pagga TERMUX SUDO
+echo ""
+echo ""
+
+
+
+echo -e "\e[91m             .andAHHAbnn."
+echo -e "           .aAHHHAAUUAAHHHAn."
+echo -e "          dHP^~^        ^~^THb."
+echo -e "    .   .AHF                YHA.   "
+echo -e "    |  .AHHb.              .dHHA.  |"
+echo -e "    |  HHAUAAHAbn      adAHAAUAHA  |"
+echo -e "    I  HF~^_____        ____ ]HHH  I"
+echo -e "   HHI HAPK^^~^YUHb  dAHHHHHHHHHH IHH"
+echo -e "   HHI HHHD> .andHH  HHUUP^~YHHHH IHH"
+echo -e "   YUI ]HHP     ^~Y  P~^     THH[ IUP"
+echo -e "    *  lHK                   ]HH^  *"
+echo -e "        THAn.  .d.aAAn.b.  .dHHP"
+echo -e "        ]HHHHAAUP^ ~~ ^YUAAHHHH["
+echo -e "        iHHP^~^  .annn.  ^~^YHHi"
+echo -e "         YHb    ~^ ^^ ^~    dHF"
+echo -e "          ^YAb..abdHHbndbndAP^"
+echo -e "           THHAAb.  .adAHHF"
+echo -e "            ^UHHHHHHHHHHU^^"
+echo -e "              ]HHUUHHHHHH["
+echo -e "            .adHHb ^HHHHHbn."
+echo -e "     ..andAAHHHHHHb.AHHHHHHHAAbnn.."
+echo -e ".ndAAHHHHHHUUHHHHHHHHHHUP^~^~^YUHHHAAbn."
+echo -e "  ^^~^YUHHP^   ^~^YUHHUP^        ^^YUP^^"
+echo -e "       ^^         ^~~^\e[0m"
+echo ""
+echo -e "-----------------------------------------"
+echo -e "\e[33m        Informática y hacking    \e[0m"
+echo -e "-----------------------------------------"
+
+echo ""
+echo ""
+echo -e "\e[32mInstalando sudo en termux...\e[0m"
+sleep 3
+pkg install ncurses-utils -y
+
+cat sudo > /data/data/com.termux/files/usr/bin/sudo
+
+chmod 700 /data/data/com.termux/files/usr/bin/sudo
+
+sleep 3s
+rm sudo
+clear
+echo -e "\e[91m               .andAHHAbnn."
+echo -e "           .aAHHHAAUUAAHHHAn."
+echo -e "          dHP^~^        ^~^THb."
+echo -e "    .   .AHF                YHA.   "
+echo -e "    |  .AHHb.              .dHHA.  |"
+echo -e "    |  HHAUAAHAbn      adAHAAUAHA  |"
+echo -e "    I  HF~^_____        ____ ]HHH  I"
+echo -e "   HHI HAPK^^~^YUHb  dAHHHHHHHHHH IHH"
+echo -e "   HHI HHHD> .andHH  HHUUP^~YHHHH IHH"
+echo -e "   YUI ]HHP     ^~Y  P~^     THH[ IUP"
+echo -e "    *  lHK                   ]HH^  *"
+echo -e "        THAn.  .d.aAAn.b.  .dHHP"
+echo -e "        ]HHHHAAUP^ ~~ ^YUAAHHHH["
+echo -e "        iHHP^~^  .annn.  ^~^YHHi"
+echo -e "         YHb    ~^ ^^ ^~    dHF"
+echo -e "          ^YAb..abdHHbndbndAP^"
+echo -e "           THHAAb.  .adAHHF"
+echo -e "            ^UHHHHHHHHHHU^^"
+echo -e "              ]HHUUHHHHHH["
+echo -e "            .adHHb ^HHHHHbn."
+echo -e "     ..andAAHHHHHHb.AHHHHHHHAAbnn.."
+echo -e ".ndAAHHHHHHUUHHHHHHHHHHUP^~^~^YUHHHAAbn."
+echo -e "  ^^~^YUHHP^   ^~^YUHHUP^        ^^YUP^^"
+echo -e "       ^^         ^~~^\e[0m"
+echo ""
+echo -e "-----------------------------------------"
+echo -e "\e[33m        Informática y hacking    \e[0m"
+echo -e "-----------------------------------------"
+echo ""
+echo ""
+echo -e "\e[32mPara verificar que su instalación este correcta "
+echo -e "\e[32mejecute sudo y otorgue permisos ROOT.\e[0m"
